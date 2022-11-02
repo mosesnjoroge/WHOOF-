df05a9018c7e79eb2d3f4ed0fef3b3209121ca22
Owen Bonertz <owenbonertz@gmail.com>
2022-11-01 21:20:37 -0400

    Dog Model Validations are partially done

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,14 @@
 class Dog < ApplicationRecord
+
   belongs_to :user
   has_many :bookings
+  validates :name, presence: true, length: { in: 3..20 }, uniqueness: true
+  validates :breed, length: { in: 3..20 }
+  #validates :skill, inclusion: { in: %w(juggling swimming skateboarding psychotherapy bomb-sniffing racing obstacle-courses fetch hiking rescue)}
+  validates :price, numericality: { only_integer: true }
+  validates :description, presence: true, length: { in: 15..280 }
+
+  def validatesSkillArr
+  end
+
 end
