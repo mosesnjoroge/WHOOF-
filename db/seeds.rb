79f389f132cb0c56e84b550f5047c9ed13cfc3d0
Owen Bonertz <owenbonertz@gmail.com>
2022-11-01 20:43:11 -0400

    Seeded and updated User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "faker"
+
+
+20.times do
+  newUser = User.new()
+  newUser.email = Faker::Internet.email
+  newUser.password = "bruhhhhhhh"
+  newUser.address = Faker::Address.street_address
+  newUser.save!
+end
