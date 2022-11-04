d1b791020d1e863d2b87d83667f2779c184ebba7
mosesnjoroge <mosesbnjoroge@outlook.com>
2022-11-04 18:38:02 -0400

    git ignore file&api integrated

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,9 @@
 class Dog < ApplicationRecord
-
   belongs_to :user
   has_many :bookings
+  has_one_attached :photo
   validates :name, presence: true, length: { in: 3..50 }, uniqueness: true
   validates :breed, length: { in: 3..20 }
   validates :price, numericality: { only_integer: true }
   validates :description, presence: true, length: { in: 15..280 }
-
 end
