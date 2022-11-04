eefa8ee3e60e56d67cb92298ecbe64073d070630
Owen Bonertz <owenbonertz@gmail.com>
2022-11-03 21:29:19 -0400

    Dog Controllers mostly done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,8 +1,13 @@
 class DogsController < ApplicationController
+
+  before_action :set_dog, only: [:show, :update, :destroy]
+
   def index
+    @dogs = Dog.all
   end
 
   def new
+    @dog = Dog.new
   end
 
   def show
@@ -12,9 +17,16 @@ class DogsController < ApplicationController
   end
 
   def destroy
+    @dog.destroy
   end
 
   private
 
-  def set_dog; end
+  def set_dog;
+    @dog = Dog.find(params[:id])
+  end
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :skills, :description, :image_url, :price, :user_id)
+  end
 end
