ffb789fe96f9a292cbbed611f7f40be49004c35a
Ricardo Gutierrez <ricardorgvillanueva@gmail.com>
2022-11-03 21:33:27 -0400

    adding bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,38 @@
 class BookingsController < ApplicationController
   def index
-  end
-
-  def new
+    @bookings = Booking.all
   end
 
   def show
+    @booking = set_booking
   end
 
+  def new
+    @booking = Booking.new()
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
   def edit
+
   end
 
-  def destroy
+  def update
+
   end
 
   private
 
-  def set_dog; end
+  def set_dog
 
-  def set_bookmark; end
+  end
+
+  def set_booking
+    Booking.find(params[:id])
+  end
+
+  def booking_params
+    params_require(:booking).permit(:startdate, :enddate)
+  end
+
 end
