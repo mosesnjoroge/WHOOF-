0ce62be71dba3f44e2f6814bef9c5d882f01bb7e
Ricardo Gutierrez <ricardorgvillanueva@gmail.com>
2022-11-04 17:11:39 -0400

    methods added to bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,23 +13,37 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
-  def edit
+    @dog = set_dog
+    @user = current_user
+    @booking.dog = @dog
+    @booking.user = @user
+    if @booking.save
+      redirect_to my_booking_path(@booking)
+    else render
+      :new
+    end
+  end
 
+  def edit
+    @booking = Booking.find(set_booking)
   end
 
   def update
-
+    @booking = Booking.find(set_booking)
+    @booking.update(params[booking_params])
+    redirect_to my_booking_path(@booking)
   end
 
   private
 
   def set_dog
-
+    Dog.find(params[:dog_id])
   end
 
   def set_booking
     Booking.find(params[:id])
   end
+
 
   def booking_params
     params_require(:booking).permit(:startdate, :enddate)
