6834e5ce338c73a12dca4aa4b2370a33c42fbdab
mosesnjoroge <mosesbnjoroge@outlook.com>
2022-11-08 00:39:42 -0500

    mydogs page is functional btn need to be added and some debugging required for edit dog controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -3,22 +3,32 @@ class DogsController < ApplicationController
   before_action :set_dog, only: [:show, :update, :destroy]
 
   def index
-
-    # @dogs = current_user.dogs
     @dogs = Dog.all
   end
 
+  # moses was here
+  def mydogs
+    @dogs = current_user.dogs
+    @dogs = Dog.all
+  end
   def new
     @dog = Dog.new
   end
 
+  # moses was here
   def show
+     @dog = set_dog
+  end
+
+  # moses was here
+  def edit
+    # @edit_dog = Dog.find(set_dog)
   end
 
   def update
-    if @dog.update(dog_params)
-      redirect_to dogs_path, notice
-    end
+    # @edit_dog = Dog.find(set_dog)
+    # @edit_dog.update(dog_params)
+    redirect_to dogs_path, notice
   end
 
   def destroy
