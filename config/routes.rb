53821a1b3e6b7df4767e5b85345b5e7c51594ca9
mosesnjoroge <mosesbnjoroge@outlook.com>
2022-12-29 17:57:35 -0500

    cart counter functional

--- config/routes.rb
+++ config/routes.rb
@@ -7,19 +7,22 @@ Rails.application.routes.draw do
   end
   # list of all user bookings
   get '/my-bookings', to: 'bookings#index', as: 'mycart'
+
   # list a booking
   get '/my-bookings/:id', to: 'bookings#show', as: 'mybooking'
+
   # list all dogs(owner)
   get '/my-dogs', to: 'dogs#mydogs', as: 'mydogs'
+
   # list a dog(owner)
   get '/my-dogs/:id', to: 'dogs#show', as: 'mydog'
+
   # remove your dog from website
   delete '/my-dogs/:id', to: "dogs#destroy", as: 'removedog'
+
   # update dog posting
   patch '/my-dogs/:id', to: 'dogs#update'
 
-  # shopping cart
-  # get '/shoppingcart', to: 'bookings#shoppingcart', as: 'cart'
   #remove your booking
   resources :bookings, only: [:destroy]
 end
