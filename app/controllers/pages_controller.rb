c53b2d84e26c010f17fff6dd811f9c3ee4a6257a
mosesnjoroge <mosesbnjoroge@outlook.com>
2022-12-31 18:47:32 -0500

    card carousel functional/styling still required

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @dogs = Dog.all.sample(9)
+    @dogs = Dog.all.sample(6)
     @dog_group1 = @dogs.first(3)
     @dog_group2 = @dogs.last(3)
   end
